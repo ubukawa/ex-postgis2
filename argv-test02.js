e7b8988fe3bc16c5c40d10efdd688cf156c653a4
ubukawa <ubukawa-t96dq@mlit.go.jp>
2023-01-06 09:58:11 -0500

    rev

--- argv-test02.js
+++ argv-test02.js
@@ -3,14 +3,19 @@
 //  }
 //use node xxx.js (tileareaNumber)
 const config = require('config')
+
+let tileList=[]
+
 if (process.argv[2]){
   if (config.get(`tileList${process.argv[2]}`)){
-    const tileList = config.get(`tileList${process.argv[2]}`)
-    console.log(tileList)
+    tileList = config.get(`tileList${process.argv[2]}`)
   } else {
     console.log('wrong tile area!!!')
   }
 } else {
-  console.log('Please specify the area')
+  console.log('Please specify the area. default is all')
+  tileList = config.get('tileList1').concat(config.get('tileList2')).concat(config.get('tileList3'))
 }
+
+console.log(tileList)
 
